8eefa69487c2d433d9af91adc12246b4ff7aa977
Dileep Ratnayake <dileep.ratnayake@silverstripe.com>
2018-11-26 11:01:09 +1300

    new extension

---
+++ src/SiteTreeExtension.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dileepratnayake
+ * Date: 26/11/18
+ * Time: 10:51 AM
+ */
+
+namespace Marcz\Swiftype;
+
+use DataExtension;
+
+class SiteTreeExtension extends DataExtension{
+    public function MetaTags($tags)
+    {
+        die("huchakke");
+    }
+}
