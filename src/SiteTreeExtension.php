7984034be675bc3f94463834aa2f7b81183e49c0
Dileep Ratnayake <dileep.ratnayake@silverstripe.com>
2018-11-26 13:59:36 +1300

    Read url segment and decide meta content depending on the segment

--- src/SiteTreeExtension.php
+++ src/SiteTreeExtension.php
@@ -10,9 +10,34 @@ namespace Marcz\Swiftype;
 
 use DataExtension;
 
+
 class SiteTreeExtension extends DataExtension{
-    public function MetaTags($tags)
+
+    public function MetaTags(&$tags)
     {
-        die("huchakke");
+        $abLink =  $this->owner->AbsoluteLink();
+        $segments = explode("/", $abLink);
+        $segment = $segments[3];
+        $meta = "";
+        switch ($segment){
+            case "business":
+                $meta = "business";
+                break;
+            case "agribusiness":
+                $meta = "agribusiness";
+                break;
+            case "wib":
+                $meta = "institutional";
+                break;
+            case "who-we-are":
+                $meta = "who-we-are";
+                break;
+            case "ask-westpac":
+                $meta = "ask-westpac";
+                break;
+            default:
+                $meta = "personal";
+        }
+        $tags .= "<meta class=\"swiftype\" name=\"type\" data-type=\"enum\" content=\"$meta\" />\n";
     }
 }
