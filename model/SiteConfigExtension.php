6b576082594f02a564fe49d39fcbd09a01dd4e53
Dileep Ratnayake <dileep.ratnayake@silverstripe.com>
2018-11-26 17:04:16 +1300

    adding siteconfig extension to manage settings

---
+++ model/SiteConfigExtension.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dileepratnayake
+ * Date: 26/11/18
+ * Time: 2:53 PM
+ */
+
+namespace Marcz\Swiftype;
+
+use DataExtension;
+
+class SiteConfigExtension extends DataExtension
+{
+
+    private static $plural_name = "Swiftype Settings";
+
+    private static $db = array(
+        'DefaultMeta' => 'Varchar',
+        'PageTypeEnabled' => 'Varchar'
+    );
+
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->addFieldsToTab('Root.Swiftype', array(
+            TextField::create('DefaultMeta','Default Meta Type Content'),
+            TextField::create('PageTypeEnabled','Enable Page Type Meta'),
+        ));
+    }
+}
