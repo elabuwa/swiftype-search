ec0e76cf6bd29558978331f94df303232a7d5a36
Dileep Ratnayake <dileep.ratnayake@silverstripe.com>
2018-11-26 17:05:28 +1300

    updating extension to accept user created values instead of hardcoding

--- src/SiteTreeExtension.php
+++ src/SiteTreeExtension.php
@@ -9,35 +9,54 @@
 namespace Marcz\Swiftype;
 
 use DataExtension;
-
+use SiteConfig;
+use PageTypeMetas;
 
 class SiteTreeExtension extends DataExtension{
 
     public function MetaTags(&$tags)
     {
-        $abLink =  $this->owner->AbsoluteLink();
-        $segments = explode("/", $abLink);
-        $segment = $segments[3];
-        $meta = "";
-        switch ($segment){
-            case "business":
-                $meta = "business";
-                break;
-            case "agribusiness":
-                $meta = "agribusiness";
-                break;
-            case "wib":
-                $meta = "institutional";
-                break;
-            case "who-we-are":
-                $meta = "who-we-are";
-                break;
-            case "ask-westpac":
-                $meta = "ask-westpac";
-                break;
-            default:
-                $meta = "personal";
+        $siteConfig = SiteConfig::current_site_config();
+        $defaultMeta = $siteConfig->DefaultMeta;
+        $PageTypeEnabled = (bool) $siteConfig->PageTypeEnabled;
+        if($PageTypeEnabled === true){
+            $abLink =  $this->owner->AbsoluteLink();
+            $segments = explode("/", $abLink);
+            $segment = $segments[3];
+            $meta = null;
+
+            $result = PageTypeMetas::get()->filter(array(
+                'segmentNumber' => 3,
+                'key' => $segment
+            ));
+            if($result->exists()){
+                $first = $result->first();
+                $meta = $first->value;
+                $tags .= "<meta class=\"swiftype\" name=\"type\" data-type=\"enum\" content=\"$meta\" />\n";
+            } else {
+                $tags .= "<meta class=\"swiftype\" name=\"type\" data-type=\"enum\" content=\"$defaultMeta\" />\n";
+            }
         }
-        $tags .= "<meta class=\"swiftype\" name=\"type\" data-type=\"enum\" content=\"$meta\" />\n";
+
+//        switch ($segment){
+//            case "business":
+//                $meta = "business";
+//                break;
+//            case "agribusiness":
+//                $meta = "agribusiness";
+//                break;
+//            case "wib":
+//                $meta = "institutional";
+//                break;
+//            case "who-we-are":
+//                $meta = "who-we-are";
+//                break;
+//            case "ask-westpac":
+//                $meta = "ask-westpac";
+//                break;
+//            default:
+//                $meta = "personal";
+//        }
+
     }
 }
